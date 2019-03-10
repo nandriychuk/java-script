7363749b362c6523246f55379099b0c18eff0d1a
Natalia <natalia1andriychuk@gmail.com>
2019-03-09 19:16:08 -0500

    adding final code

--- static/js/app.js
+++ static/js/app.js
@@ -4,9 +4,13 @@ var tableData = data;
 // grab references to the output
 var tableBody = d3.select("tbody")
 
+// Select filter field
+var filterField = d3.select(".filter");
+
 // Select the submit button
 var filterButton = d3.select("#filter-btn");
 
+// initialize data table before filtering
 tableData.forEach((ufoData) => {
     var row = tableBody.append("tr");
     Object.entries(ufoData).forEach(([key,value]) => {
@@ -15,22 +19,49 @@ tableData.forEach((ufoData) => {
     });
 });
 
+//Create Event Listener
 filterButton.on("click", function(){
+
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
-    // Select the input element 
-    var inputElement = d3.select(".form-control");
+    // // Select all input element 
+    var date_inputElement = d3.select("#datetime");
+    var city_inputElement = d3.select("#city");
+    var state_inputElement = d3.select("#state");
+    var country_inputElement = d3.select("#country");
+    var shape_inputElement = d3.select("#shape");
 
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value")
+    // Get the value property of all input element and convert them to lower case
+    // for cases where user uses capital letters
+    var dateElement = date_inputElement.property("value");
+    var cityElement = city_inputElement.property("value").toLowerCase();
+    var stateElement = state_inputElement.property("value").toLowerCase();
+    var countryElement = country_inputElement.property("value").toLowerCase();
+    var shapeElement = shape_inputElement.property("value").toLowerCase();
+
+    //Create if statements to filter data
+    var filteredData = data;
+    if(dateElement !== ""){
+        filteredData = filteredData.filter(row => row.datetime === dateElement)
+    }
+    if(cityElement !== ""){
+        filteredData = filteredData.filter(row => row.city === cityElement)
+    }
+    if(stateElement !== ""){
+        filteredData = filteredData.filter(row => row.state === stateElement)
+    }
+    if(countryElement !== ""){
+        filteredData = filteredData.filter(row => row.country === countryElement)
+    }
+    if(shapeElement !== ""){
+        filteredData = filteredData.filter(row => row.shape === shapeElement)
+    }
 
     // clear the existing output
     tableBody.html("");
 
-    // Define filter
-    var filteredData = tableData.filter(date => date.datetime === inputValue);
-
+    //Pass filtered data to data table and display it
     filteredData.forEach((ufoData) => {
         var row = tableBody.append("tr");
         Object.entries(ufoData).forEach(([key,value]) => {
@@ -38,55 +69,6 @@ filterButton.on("click", function(){
             tableData.text(value);
         });
     });
-})
-
-// d3.selectAll(".table-head")
-//       .on("mouseover", function(){
-//           d3.select(this)
-//             .style("background-color", "orange");
-
-//           // Get current event info
-//           console.log(d3.event);
-          
-//           // Get x & y co-ordinates
-//           console.log(d3.mouse(this));
-//       })
-//       .on("mouseout", function(){
-//           d3.select(this)
-//             .style("background-color", "#1b3146")
-//       });
+});
 
 
-// YOUR CODE HERE!
-
-
-
-
-// tableData.forEach((ufoData) => {
-//     var row = tableBody.append("tr");
-//     Object.entries(ufoData).forEach(([key,value]) => {
-//         var tableData = tableBody.append("td");
-//         tableData.text(value);
-//     });
-// });
-
-// var filteredData = 
-
-
-
-
-
-
-// // Step 5: Use d3 to update each cell's text with
-// // weather report values (weekday, date, high, low)
-// data.forEach(function(weatherReport) {
-//     console.log(weatherReport);
-//     var row = tbody.append("tr");
-//     Object.entries(weatherReport).forEach(function([key, value]) {
-//       console.log(key, value);
-//       // Append a cell to the row for each value
-//       // in the weather report object
-//       var cell = tbody.append("td");
-//       cell.text(value);
-//     });
-//   });
